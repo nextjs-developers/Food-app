05b4a0961cfedc7538aa223bdc3a3fb5d678d366
Neda Vaheb <n.vaheb.nv@gmail.com>
2025-03-26 22:28:25 -0700

    Login button and model created

---
+++ src/components/partials/container/ModalContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ModalContainer({ children, isOpen }) {
+
+    if(!isOpen) return;
+  return (
+    <div className="modal modal-open fixed inset-0 z-10">
+    <div className="modal-backdrop bg-black/20 backdrop-blur-sm"></div>
+    <div className="modal-box w-full h-full max-w-none rounded-none flex items-center justify-center bg-transparent shadow-none">
+      {children}
+    </div>
+  </div>
+  )
+}
+
+export default ModalContainer
