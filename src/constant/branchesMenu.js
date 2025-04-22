ed1b9d176be93554059a1c92f9632d5b5ed2746d
Mehdi Ehsani <metti.work@gmail.com>
2025-04-22 18:55:45 +0330

    feat(data): add branchesMenu data to project

---
+++ src/constant/branchesMenu.js
@@ -0,0 +1,182 @@
+export const branchesMenu = [
+    {
+        title: "Whole Wheat Pizza Crust", 
+        rate: 4,
+        price: 23,
+        image:"/mainCourse/Amazing Whole Wheat Pizza Crust.webp",
+        discount: 2,
+        priceAfterDiscount: price - discount,
+        category: "mainCourse"
+    },
+    {
+        title: "American Lasagna", 
+        rate: 3,
+        price: 19,
+        image:"/mainCourse/American Lasagna.webp",
+        discount: 1,
+        priceAfterDiscount: price - discount,
+        category: "mainCourse"
+    },
+    {
+        title: "Balsamic Chicken and Pasta", 
+        rate: 3,
+        price: 19,
+        image:"/mainCourse/Balsamic Chicken and Pasta.webp",
+        discount: 1,
+        priceAfterDiscount: price - discount,
+        category: "mainCourse"
+    },
+    {
+        title: "BBQ Chicken Pizza", 
+        rate: 3,
+        price: 19,
+        image:"/mainCourse/BBQ Chicken Pizza.webp",
+        discount: 1,
+        priceAfterDiscount: price - discount,
+        category: "mainCourse"
+    },
+    {
+        title: "Black Bean Lasagna", 
+        rate: 3,
+        price: 19,
+        image:"/mainCourse/Black Bean Lasagna.webp",
+        discount: 1,
+        priceAfterDiscount: price - discount,
+        category: "mainCourse"
+    },
+    {
+        title: "Cranberry Wassail", 
+        rate: 5,
+        price: 4,
+        image:"/drinks/Cranberry Wassail.webp",
+        discount: 0.4,
+        priceAfterDiscount: price - discount,
+        category: "drinks"
+    },
+    {
+        title: "Eggnog Latte", 
+        rate: 5,
+        price: 4,
+        image:"/drinks/Eggnog Latte.webp",
+        discount: 0.4,
+        priceAfterDiscount: price - discount,
+        category: "drinks"
+    },
+    {
+        title: "Gingerade", 
+        rate: 5,
+        price: 4,
+        image:"/drinks/Gingerade.webp",
+        discount: 0.4,
+        priceAfterDiscount: price - discount,
+        category: "drinks"
+    },
+    {
+        title: "Green Punch", 
+        rate: 5,
+        price: 4,
+        image:"/drinks/Green Punch.webp",
+        discount: 0.4,
+        priceAfterDiscount: price - discount,
+        category: "drinks"
+    },
+    {
+        title: "Hot Buttered Rum", 
+        rate: 5,
+        price: 4,
+        image:"/drinks/Hot Buttered Rum.webp",
+        discount: 0.4,
+        priceAfterDiscount: price - discount,
+        category: "drinks"
+    },
+    {
+        title: "Apple Crisp", 
+        rate: 4,
+        price: 8,
+        image:"/desserts/Apple Crisp.webp",
+        discount: 1.4,
+        priceAfterDiscount: price - discount,
+        category: "desserts"
+    },
+    {
+        title: "Cinnamon Nutmeg Coffee Cake", 
+        rate: 4,
+        price: 8,
+        image:"/desserts/Cinnamon Nutmeg Coffee Cake.webp",
+        discount: 1.4,
+        priceAfterDiscount: price - discount,
+        category: "desserts"
+    },
+    {
+        title: "Coconut Milk Ice Cream", 
+        rate: 4,
+        price: 8,
+        image:"/desserts/Coconut Milk Ice Cream.webp",
+        discount: 1.4,
+        priceAfterDiscount: price - discount,
+        category: "desserts"
+    },
+    {
+        title: "Cottage Cheese Blueberry Breakfast Cake", 
+        rate: 4,
+        price: 8,
+        image:"/desserts/Cottage Cheese Blueberry Breakfast Cake.webp",
+        discount: 1.4,
+        priceAfterDiscount: price - discount,
+        category: "desserts"
+    },
+    {
+        title: "No-Bake Millionaire’s Shortbread", 
+        rate: 4,
+        price: 8,
+        image:"/desserts/No-Bake Millionaire’s Shortbread.webp",
+        discount: 1.4,
+        priceAfterDiscount: price - discount,
+        category: "desserts"
+    },
+    {
+        title: "Apple Spinach Salad", 
+        rate: 5,
+        price: 12,
+        image:"/appetizers/Apple Spinach Salad.webp",
+        discount: 1,
+        priceAfterDiscount: price - discount,
+        category: "appetizers"
+    },
+    {
+        title: "Artichoke Parmesan Soup", 
+        rate: 5,
+        price: 12,
+        image:"/appetizers/Artichoke Parmesan Soup.webp",
+        discount: 1,
+        priceAfterDiscount: price - discount,
+        category: "appetizers"
+    },
+    {
+        title: "Carrot Raisin Salad", 
+        rate: 5,
+        price: 12,
+        image:"/appetizers/Carrot Raisin Salad.webp",
+        discount: 1,
+        priceAfterDiscount: price - discount,
+        category: "appetizers"
+    },
+    {
+        title: "Cheesy Taco Soup", 
+        rate: 5,
+        price: 12,
+        image:"/appetizers/Cheesy Taco Soup.webp",
+        discount: 1,
+        priceAfterDiscount: price - discount,
+        category: "appetizers"
+    },
+    {
+        title: "Creamy Tomato Bean Soup", 
+        rate: 5,
+        price: 12,
+        image:"/appetizers/Creamy Tomato Bean Soup.webp",
+        discount: 1,
+        priceAfterDiscount: price - discount,
+        category: "appetizers"
+    },
+]
